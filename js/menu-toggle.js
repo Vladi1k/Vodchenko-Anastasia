1ad92b0e3850dafab8d3dfe17e15c1285b8531fc
Vlad <beymip3@gmail.com>
2024-12-20 15:33:06 +0200

    1

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,59 +1,59 @@
+// Обработчики подменю
 const toggleMenus = document.querySelectorAll(".menu-toggle");
 
 toggleMenus.forEach((toggleMenu) => {
-  toggleMenu.addEventListener("click", (e) => {
-    e.preventDefault();
+    toggleMenu.addEventListener("click", (e) => {
+        e.preventDefault();
 
-    const currentSubmenu = toggleMenu.nextElementSibling; 
+        const currentSubmenu = toggleMenu.nextElementSibling;
 
-    document.querySelectorAll(".submenu").forEach((submenu) => {
-      if (submenu !== currentSubmenu) {
-        submenu.classList.remove("visible");
-        submenu.classList.add("hidden");
-      }
+        toggleMenus.forEach((menu) => {
+            const submenu = menu.nextElementSibling;
+            if (submenu && submenu !== currentSubmenu) {
+                submenu.classList.remove('visible');
+                submenu.classList.add('hidden');
+            }
+        });
+
+        if (currentSubmenu) {
+            currentSubmenu.classList.toggle('visible');
+            currentSubmenu.classList.toggle('hidden');
+        }
     });
-
-    if (currentSubmenu) {
-      currentSubmenu.classList.toggle("visible");
-      currentSubmenu.classList.toggle("hidden");
-    }
-  });
 });
 
-const mobileNav = document.querySelector(".mobileNav")
-const mobileMenuLink = document.querySelector(".mobileMenuLink")
+// Мобильное меню
+const mobileNav = document.querySelector(".mobileNav");
+const mobileMenuLink = document.querySelector(".mobileMenuLink");
 
-mobileMenuLink.addEventListener("click", (e) => {
-  mobileNav.classList.toggle("menu-open")
-})
+mobileMenuLink.addEventListener("click", () => {
+    mobileNav.classList.toggle("menu-open");
+});
 
+// Обработчик медиа-запросов
 const targetElement = document.querySelector('.thumbnails');
 const thumbnailsImgg = document.querySelectorAll(".thumbnails-img");
 const mediaQuery = window.matchMedia('(max-width: 800px)');
 
 function handleMediaQueryChange(event) {
     if (event.matches) {
-        
         targetElement.classList.remove('stacked');
-        targetElement.classList.add("gallery");
-        thumbnailsImgg.forEach((thumb, i) => {
-          const realSrc = thumb.getAttribute("data-src");
-          if (realSrc) { 
-              thumb.setAttribute("src", realSrc);
-              thumb.addEventListener("load", () => {
-                  thumb.classList.remove("preload");
-              });
-          }
-        })
+        targetElement.classList.add('gallery');
+        thumbnailsImgg.forEach((thumb) => {
+            const realSrc = thumb.getAttribute('data-src');
+            if (realSrc && !thumb.src) {
+                thumb.src = realSrc;
+                thumb.addEventListener('load', () => {
+                    thumb.classList.remove('preload');
+                });
+            }
+        });
     } else {
-        
-        mobileNav.classList.remove("menu-open")
+        mobileNav.classList.remove('menu-open');
         targetElement.classList.add('stacked');
-        targetElement.classList.remove("gallery")
+        targetElement.classList.remove('gallery');
     }
 }
 
 mediaQuery.addEventListener('change', handleMediaQueryChange);
-
 handleMediaQueryChange(mediaQuery);
-
--- js/slider.js
+++ js/slider.js
@@ -8,56 +8,56 @@ const thumbnailsImg = document.querySelectorAll(".thumbnails-img");
 
 let index = 0; 
 
-
 function updateSlide(newIndex) {
     img[index].classList.remove("active"); 
     img[index].classList.add("loaded"); 
-
     index = newIndex; 
-
     img[index].classList.remove("loaded"); 
     img[index].classList.add("active"); 
 }
-
 
 rightControl.addEventListener("click", () => {
     const nextIndex = (index < img.length - 1) ? index + 1 : 0;
     updateSlide(nextIndex);
 });
 
-
 leftControl.addEventListener("click", () => {
     const prevIndex = (index > 0) ? index - 1 : img.length - 1;
     updateSlide(prevIndex);
 });
 
-centerControl.addEventListener("click", () => {
-    slideShow.classList.add("stacked");
-    thumbnails.classList.remove("stacked");
-    leftControl.classList.add("stacked");
-    rightControl.classList.add("stacked");
-    centerControl.classList.add("stacked");
-
-    thumbnails.classList.add("gallery");
-
-    thumbnailsImg.forEach((thumb, i) => {
-        const realSrc = thumb.getAttribute("data-src");
-        if (realSrc) { 
-            thumb.setAttribute("src", realSrc);
-            thumb.addEventListener("load", () => {
-                thumb.classList.remove("preload");
-            });
-        }
-
-        thumb.addEventListener("click", () => {
-            thumbnails.classList.add("stacked");
-            thumbnails.classList.remove("gallery");
-            slideShow.classList.remove("stacked");
-            leftControl.classList.remove("stacked");
-            rightControl.classList.remove("stacked");
-            centerControl.classList.remove("stacked");
-
-            updateSlide(i); 
-        });
+// Настраиваем миниатюры только один раз
+thumbnailsImg.forEach((thumb, i) => {
+    thumb.addEventListener("click", () => {
+        toggleGalleryMode(false);
+        updateSlide(i);
     });
 });
+
+// Lazy-load изображений
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const img = entry.target;
+            const realSrc = img.getAttribute("data-src");
+            if (realSrc) {
+                img.src = realSrc;
+                img.classList.remove("preload");
+                observer.unobserve(img);
+            }
+        }
+    });
+}, { rootMargin: "50px" });
+
+thumbnailsImg.forEach((img) => observer.observe(img));
+
+function toggleGalleryMode(showGallery) {
+    slideShow.classList.toggle("stacked", showGallery);
+    thumbnails.classList.toggle("stacked", !showGallery);
+    thumbnails.classList.toggle("gallery", showGallery);
+    leftControl.classList.toggle("stacked", showGallery);
+    rightControl.classList.toggle("stacked", showGallery);
+    centerControl.classList.toggle("stacked", showGallery);
+}
+
+centerControl.addEventListener("click", () => toggleGalleryMode(true));
